7f8da6af179f122dd47f4d7ded42403c50ec4479
Kyle-Gonzales-Github <kylegonzales@oakland.edu>
2022-09-20 17:03:55 -0400

    Another last commit!

--- src/me/kylegonzales/helloworld/Main.java
+++ src/me/kylegonzales/helloworld/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world again!");
+		System.out.println("I am the best!");
 		update();
 	}
-	
 	public static void update() {
 		System.out.println("Update I love updates!");
 	}
